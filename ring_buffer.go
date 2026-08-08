2319a543732fc0a0b18b7a5b8bfa67d6d9fe4b06
501urchin <jaychhitu@gmail.com>
2026-08-08 14:10:13 +0200

    updated ring buf to acccept any type

--- ring_buffer.go
+++ ring_buffer.go
@@ -11,14 +11,14 @@ type NumberConstraint interface {
 		~float32 | ~float64
 }
 
-type RingBuffer[T NumberConstraint] struct {
+type RingBuffer[T any] struct {
 	cap     int
 	headIdx int
 	buf     []T
 }
 
 // NewRingBuffer created a new ring buffer. note that the ring buffer is not thread safe
-func NewRingBuffer[T NumberConstraint](cap int, initialValues ...T) *RingBuffer[T] {
+func NewRingBuffer[T any](cap int, initialValues ...T) *RingBuffer[T] {
 	if cap <= 0 {
 		panic(fmt.Errorf("invalid capacity: excpect a cap greater than 0 but got %d", cap))
 	}
@@ -90,11 +90,73 @@ func (rb *RingBuffer[T]) Len() int {
 	return len(rb.buf)
 }
 func (rb *RingBuffer[T]) Max() T {
-	return slices.Max(rb.buf)
+	var zero T
+
+	switch v := any(rb.buf).(type) {
+	case []int:
+		return any(slices.Max(v)).(T)
+	case []int8:
+		return any(slices.Max(v)).(T)
+	case []int16:
+		return any(slices.Max(v)).(T)
+	case []int32:
+		return any(slices.Max(v)).(T)
+	case []int64:
+		return any(slices.Max(v)).(T)
+	case []uint:
+		return any(slices.Max(v)).(T)
+	case []uint8:
+		return any(slices.Max(v)).(T)
+	case []uint16:
+		return any(slices.Max(v)).(T)
+	case []uint32:
+		return any(slices.Max(v)).(T)
+	case []uint64:
+		return any(slices.Max(v)).(T)
+	case []float32:
+		return any(slices.Max(v)).(T)
+	case []float64:
+		return any(slices.Max(v)).(T)
+	case []string:
+		return any(slices.Max(v)).(T)
+	default:
+		return zero
+	}
 }
 
 func (rb *RingBuffer[T]) Min() T {
-	return slices.Min(rb.buf)
+	var zero T
+
+	switch v := any(rb.buf).(type) {
+	case []int:
+		return any(slices.Min(v)).(T)
+	case []int8:
+		return any(slices.Min(v)).(T)
+	case []int16:
+		return any(slices.Min(v)).(T)
+	case []int32:
+		return any(slices.Min(v)).(T)
+	case []int64:
+		return any(slices.Min(v)).(T)
+	case []uint:
+		return any(slices.Min(v)).(T)
+	case []uint8:
+		return any(slices.Min(v)).(T)
+	case []uint16:
+		return any(slices.Min(v)).(T)
+	case []uint32:
+		return any(slices.Min(v)).(T)
+	case []uint64:
+		return any(slices.Min(v)).(T)
+	case []float32:
+		return any(slices.Min(v)).(T)
+	case []float64:
+		return any(slices.Min(v)).(T)
+	case []string:
+		return any(slices.Min(v)).(T)
+	default:
+		return zero
+	}
 }
 
 func (rb *RingBuffer[T]) BackingSlice() []T {
